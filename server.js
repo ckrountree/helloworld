e89ee3dd7682e3a5fd3d890d9bd9276a1a7d06e7
ckrountree <jmail0207@gmail.com>
2017-10-15 14:00:20 -0700

    create server

---
+++ server.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+var http = require("http");
+
+http.createServer(function(request, response) {
+response.writeHead(200, {"Content-Type": "text/plain"});
+response.write("Hello one World");
+response.end();
+}).listen(8888);
